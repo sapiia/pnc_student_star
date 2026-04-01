370fb621bb04f11b3eb666c58b488235cdd397fd
Sophy Moeurn <moeurnsophy92@gmail.com>
2026-04-01 17:42:17 +0700

    fix/email sixth

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import useScrollReveal from '../hooks/useScrollReveal';
 import LandingPage from '../features/shared/LandingPage';
